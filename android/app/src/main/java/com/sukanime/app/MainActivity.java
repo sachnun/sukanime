f70d35d010861e417200c7343b52d0ba4f81ed68
Sachnun <sachnun12@gmail.com>
2025-12-04 04:46:14 +0000

    fix: change onDestroy to public to match BridgeActivity

--- android/app/src/main/java/com/sukanime/app/MainActivity.java
+++ android/app/src/main/java/com/sukanime/app/MainActivity.java
@@ -75,7 +75,7 @@ public class MainActivity extends BridgeActivity {
     }
     
     @Override
-    protected void onDestroy() {
+    public void onDestroy() {
         super.onDestroy();
         if (downloadReceiver != null) {
             unregisterReceiver(downloadReceiver);
