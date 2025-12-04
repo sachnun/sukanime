701132328c72288646293ec24a4e53e15fbe2ac8
Sachnun <sachnun12@gmail.com>
2025-12-04 01:32:26 +0000

    fix: handle Android back button to navigate instead of exit
    - Add @capacitor/app plugin
    - Override onBackPressed in MainActivity to go back in WebView history
    - Only exit app when there's no history to go back to

--- android/app/src/main/java/com/sukanime/app/MainActivity.java
+++ android/app/src/main/java/com/sukanime/app/MainActivity.java
@@ -1,5 +1,17 @@
 package com.sukanime.app;
 
+import android.os.Bundle;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onBackPressed() {
+        WebView webView = getBridge().getWebView();
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
