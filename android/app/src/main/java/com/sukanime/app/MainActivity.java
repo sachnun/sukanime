6b794abe22e51f99b50fd30df3226c35b7fbea66
Sachnun <sachnun12@gmail.com>
2025-12-04 02:18:12 +0000

    fix: use XML theme for status bar instead of Java code
    - Change splash theme parent from Theme.SplashScreen to AppTheme.NoActionBar
    - Add windowDrawsSystemBarBackgrounds to theme
    - Remove Java status bar code (XML is more reliable in release builds)

--- android/app/src/main/java/com/sukanime/app/MainActivity.java
+++ android/app/src/main/java/com/sukanime/app/MainActivity.java
@@ -1,9 +1,6 @@
 package com.sukanime.app;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.view.View;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import androidx.activity.OnBackPressedCallback;
 import com.getcapacitor.BridgeActivity;
@@ -13,24 +10,7 @@ public class MainActivity extends BridgeActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        // Make status bar visible with dark background
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            getWindow().setStatusBarColor(0xFF0a0a0a);
-            getWindow().setNavigationBarColor(0xFF0a0a0a);
-        }
-        
-        // Set light status bar icons (white icons on dark background)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            View decorView = getWindow().getDecorView();
-            // Clear SYSTEM_UI_FLAG_LIGHT_STATUS_BAR to get white icons
-            decorView.setSystemUiVisibility(
-                decorView.getSystemUiVisibility() & ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR
-            );
-        }
-        
-        // Handle back button with modern callback
+        // Handle back button
         getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
             @Override
             public void handleOnBackPressed() {
@@ -38,7 +18,6 @@ public class MainActivity extends BridgeActivity {
                 if (webView != null && webView.canGoBack()) {
                     webView.goBack();
                 } else {
-                    // Exit the app
                     finish();
                 }
             }
